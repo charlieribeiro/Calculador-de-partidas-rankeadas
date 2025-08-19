01e8d408d843849840bfd3bfdd6ff8e0eaf77a73
charlieribeiro <charlie.ribeiro@outlook.com.br>
2025-08-19 14:40:32 -0300

    Criação das funções para informar o saldo e o nivel

---
+++ README.md
@@ -0,0 +1,1 @@
+# Calculadora de vitórias ranqueadas
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+//Calculadora de Vitórias Ranqueadas
 
-function determinarNivel(win) {
-  if (win <= 10) return "Ferro";
-  if (win <= 20) return "Bronze";
-  if (win <= 50) return "Prata";
-  if (win <= 80) return "Ouro";
-  if (win <= 90) return "Diamante";
-  if (win <= 100) return "Lendário";
+//Função que calcula a diferença entre vitorias e derrotas
+function calcularPontos(vitorias, derrotas) {
+  return vitorias - derrotas;
+}
+
+//Função que determina o nível do héroi
+function determinarNivel(vitorias) {
+  if (vitorias <= 10)
+    return "Ferro";
+  if (vitorias <= 20)
+    return "Bronze";
+  if (vitorias <= 50)
+    return "Prata";
+  if (vitorias <= 80)
+    return "Ouro";
+  if (vitorias <= 90)
+    return "Diamante";
+  if (vitorias <= 100)
+    return "Lendário";
   return "Imortal";
 }
+
+//Função principal
+function main(vitorias, derrotas) {
+  const pontos = calcularPontos(vitorias, derrotas);
+  const nivel = determinarNivel(vitorias);
+
+  console.log(`O Herói tem de saldo de ${pontos} está no nível de ${nivel}.`);
+}
+
+//Teste da função principal com diferentes valores
+main(30,5)
+main(150,40)
+main(85,20)
